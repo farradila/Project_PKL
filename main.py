8cae7264fdeaaa54c558e862db22b5aea520ebb6
farradila <56152324+farradila@users.noreply.github.com>
2021-07-25 19:05:37 +0700

    Update 2

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-from flask import Flask, render_template, request, make_response
+from flask import Flask, render_template, request, make_response, redirect
 import sqlite3
 from datetime import datetime
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 import io
+import os
+import time
+import pandas as pd
+import numpy as np
+from werkzeug.utils import secure_filename
+from flask_uploads import UploadSet,configure_uploads,IMAGES,DATA,ALL
 
-from application.models import IncomeExpenses
+# ML Packages
+from sklearn import model_selection
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
 
 app = Flask(__name__)
 
@@ -97,7 +110,19 @@ def monitoring():
     }
     return render_template('monitoring.html', **templateData)
 
-@app.route('/monitoring', methods=['POST'])
+@app.route("/grafik")
+def grafik():
+    time, temp, hum = getLastData()
+    templateData = {
+        'time': time,
+        'temp': temp,
+        'hum': hum,
+        'freq': freqSamples,
+        'rangeTime': rangeTime
+    }
+    return render_template('grafik.html', **templateData)
+
+@app.route('/grafik', methods=['POST'])
 def my_form_post():
     global numSamples
     global freqSamples
@@ -119,8 +144,7 @@ def my_form_post():
         'freq'		: freqSamples,
         'rangeTime'	: rangeTime
     }
-    return render_template('index.html', **templateData)
-
+    return render_template('grafik.html', **templateData)
 
 @app.route('/plot/temp')
 def plot_temp():
@@ -158,22 +182,96 @@ def plot_hum():
     response.mimetype = 'image/png'
     return response
 
-@app.route("/grafik")
-def grafik():
-    time, temp, hum = getLastData()
-    templateData = {
-        'time': time,
-        'temp': temp,
-        'hum': hum,
-        'freq': freqSamples,
-        'rangeTime': rangeTime
-    }
-    return render_template('grafik.html', **templateData)
-
 @app.route("/table")
 def table():
-    entries = IncomeExpenses.query.order_by(IncomeExpenses.date.desc()).all()
-    return render_template("table.html", entries=entries)
+    conn = sqlite3.connect('sensorsData.db')
+    conn.row_factory = sqlite3.Row
+    curs = conn.cursor()
+    curs.execute("select * from data_pencemaran")
+    rows = curs.fetchall();
+
+    return render_template("table.html",rows = rows)
+
+@app.route("/delete", methods=['POST','GET'])
+def delete(id):
+    conn = sqlite3.connect('sensorsData.db')
+    curs = conn.cursor()
+    curs.execute("DELETE FROM data_pencemaran WHERE id=%s", (id,))
+    return render_template("table.html")
+
+db = sqlite3(app)
+
+files = UploadSet('files',ALL)
+app.config['UPLOADED_FILES_DEST'] = 'static/uploadsDB'
+configure_uploads(app,files)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///static/uploadsDB/filestorage.db'
+
+class FileContents(db.Model):
+	id = db.Column(db.Integer,primary_key=True)
+	name = db.Column(db.String(300))
+	modeldata = db.Column(db.String(300))
+	data = db.Column(db.LargeBinary)
+
+@app.route("/grafik2", methods=['GET','POST'])
+def dataupload():
+    if request.method == 'POST' and 'csv_data' in request.files:
+        file = request.files['csv_data']
+        filename = secure_filename(file.filename)
+
+        file.save(os.path.join('static/uploadsDB', filename))
+        fullfile = os.path.join('static/uploadsDB', filename)
+
+        # For Time
+        date = str(datetime.datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d %H:%M:%S"))
+
+        # EDA function
+        df = pd.read_csv(os.path.join('static/uploadsDB', filename))
+        df_size = df.size
+        df_shape = df.shape
+        df_columns = list(df.columns)
+        df_targetname = df[df.columns[-1]].name
+        df_featurenames = df_columns[0:-1]  # select all columns till last column
+        df_Xfeatures = df.iloc[:, 0:-1]
+        df_Ylabels = df[df.columns[-1]]  # Select the last column as target
+        # same as above df_Ylabels = df.iloc[:,-1]
+
+        # Model Building
+        X = df_Xfeatures
+        Y = df_Ylabels
+        seed = 7
+        # prepare models
+        models = []
+        models.append(('LR', LogisticRegression()))
+        models.append(('LDA', LinearDiscriminantAnalysis()))
+        models.append(('KNN', KNeighborsClassifier()))
+        models.append(('CART', DecisionTreeClassifier()))
+        models.append(('NB', GaussianNB()))
+        models.append(('SVM', SVC()))
+        # evaluate each model in turn
+
+        results = []
+        names = []
+        allmodels = []
+        scoring = 'accuracy'
+        for name, model in models:
+            kfold = model_selection.KFold(n_splits=10, shuffle=True)
+            cv_results = model_selection.cross_val_score(model, X, Y, cv=kfold, scoring=scoring)
+            results.append(cv_results)
+            names.append(name)
+            msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+            allmodels.append(msg)
+            model_results = results
+            model_names = names
+
+    return render_template('grafik2.html',filename=filename,date=date,
+		df_size=df_size,
+		df_shape=df_shape,
+		df_columns =df_columns,
+		df_targetname =df_targetname,
+		model_results = allmodels,
+		model_names = names,
+		fullfile = fullfile,
+		dfplot = df)
 
 if __name__ == "__main__":
     app.run(debug=True)
