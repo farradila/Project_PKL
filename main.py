8750bdda74529cc01e0463c58f2bd744019e8537
farradila <56152324+farradila@users.noreply.github.com>
2021-07-28 14:12:46 +0700

    Update 3

--- main.py
+++ main.py
@@ -11,14 +11,11 @@ import numpy as np
 from werkzeug.utils import secure_filename
 from flask_uploads import UploadSet,configure_uploads,IMAGES,DATA,ALL
 
-# ML Packages
-from sklearn import model_selection
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
 
 app = Flask(__name__)
 
@@ -182,96 +179,69 @@ def plot_hum():
     response.mimetype = 'image/png'
     return response
 
+files = UploadSet('files', ALL)
+app.config['UPLOADED_FILES_DEST'] = 'static/uploadsDB'
+configure_uploads(app, files)
+
 @app.route("/table")
 def table():
-    conn = sqlite3.connect('sensorsData.db')
-    conn.row_factory = sqlite3.Row
-    curs = conn.cursor()
-    curs.execute("select * from data_pencemaran")
-    rows = curs.fetchall();
+    return render_template('table.html')
 
-    return render_template("table.html",rows = rows)
-
-@app.route("/delete", methods=['POST','GET'])
-def delete(id):
-    conn = sqlite3.connect('sensorsData.db')
-    curs = conn.cursor()
-    curs.execute("DELETE FROM data_pencemaran WHERE id=%s", (id,))
-    return render_template("table.html")
-
-db = sqlite3(app)
-
-files = UploadSet('files',ALL)
-app.config['UPLOADED_FILES_DEST'] = 'static/uploadsDB'
-configure_uploads(app,files)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///static/uploadsDB/filestorage.db'
-
-class FileContents(db.Model):
-	id = db.Column(db.Integer,primary_key=True)
-	name = db.Column(db.String(300))
-	modeldata = db.Column(db.String(300))
-	data = db.Column(db.LargeBinary)
-
-@app.route("/grafik2", methods=['GET','POST'])
+@app.route("/dataupload", methods=['GET', 'POST'])
 def dataupload():
     if request.method == 'POST' and 'csv_data' in request.files:
         file = request.files['csv_data']
         filename = secure_filename(file.filename)
-
         file.save(os.path.join('static/uploadsDB', filename))
-        fullfile = os.path.join('static/uploadsDB', filename)
 
-        # For Time
-        date = str(datetime.datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d %H:%M:%S"))
+        date = str(datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d %H:%M:%S"))
 
         # EDA function
-        df = pd.read_csv(os.path.join('static/uploadsDB', filename))
+        df = pd.read_csv(os.path.join('static/uploadsDB',filename))
         df_size = df.size
         df_shape = df.shape
         df_columns = list(df.columns)
         df_targetname = df[df.columns[-1]].name
-        df_featurenames = df_columns[0:-1]  # select all columns till last column
-        df_Xfeatures = df.iloc[:, 0:-1]
-        df_Ylabels = df[df.columns[-1]]  # Select the last column as target
-        # same as above df_Ylabels = df.iloc[:,-1]
 
-        # Model Building
-        X = df_Xfeatures
-        Y = df_Ylabels
-        seed = 7
-        # prepare models
-        models = []
-        models.append(('LR', LogisticRegression()))
-        models.append(('LDA', LinearDiscriminantAnalysis()))
-        models.append(('KNN', KNeighborsClassifier()))
-        models.append(('CART', DecisionTreeClassifier()))
-        models.append(('NB', GaussianNB()))
-        models.append(('SVM', SVC()))
-        # evaluate each model in turn
+    return render_template('details.html', filename=filename, date=date,
+                           df_size=df_size,
+                           df_shape=df_shape,
+                           df_columns=df_columns,
+                           df_targetname=df_targetname,
+                           dfplot=df)
 
-        results = []
-        names = []
-        allmodels = []
-        scoring = 'accuracy'
-        for name, model in models:
-            kfold = model_selection.KFold(n_splits=10, shuffle=True)
-            cv_results = model_selection.cross_val_score(model, X, Y, cv=kfold, scoring=scoring)
-            results.append(cv_results)
-            names.append(name)
-            msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
-            allmodels.append(msg)
-            model_results = results
-            model_names = names
+@app.route("/prediksi")
+def prediksi():
+    return render_template('prediksi.html')
 
-    return render_template('grafik2.html',filename=filename,date=date,
-		df_size=df_size,
-		df_shape=df_shape,
-		df_columns =df_columns,
-		df_targetname =df_targetname,
-		model_results = allmodels,
-		model_names = names,
-		fullfile = fullfile,
-		dfplot = df)
+@app.route("/result", methods = ['POST', 'GET'])
+def result():
+    if request.method == 'POST' and 'csv_data' in request.files:
+        file = request.files['csv_data']
+        teks = pd.read_csv(file, header=0, delimiter=',', encoding='utf-8')
+        df = pd.DataFrame(teks)
+
+        xTarget = df.drop(['tanggal', 'critical', 'categori', 'lokasi_spku'], axis=1)
+
+        yTarget = df['categori']
+
+        labelencoder_class = LabelEncoder()
+        Y = labelencoder_class.fit_transform(yTarget)
+        tfidf_transformer = OneHotEncoder()
+        X = tfidf_transformer.fit_transform(xTarget)
+
+        x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=1)
+
+        clf = SVC()
+        clf = clf.fit(x_train, y_train)
+        y_pred = clf.predict(x_test)
+        result = accuracy_score(y_pred, y_test)
+        kategori = "Sedang" if result > 0.5 else "Tidak Sehat"
+        return render_template("prediksi2.html", result=result, kategori=kategori)
+
+@app.route("/summary")
+def summary():
+    return render_template("summary.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
